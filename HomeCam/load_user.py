3b6664e0f3cb730d879cb55606ff38cc2d89f0f6
su11ji04 <113424325+su11ji04@users.noreply.github.com>
2025-05-30 08:13:55 +0900

    Create load_user.py

---
+++ HomeCam/load_user.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def load_user_profile(csv_path: str, user_id: str):
+    user_df = pd.read_csv(csv_path, encoding='cp949')
+    user_info = user_df[user_df['user_id'] == user_id].iloc[0]
+
+    return {
+        "child_age": user_info['child_age'],
+        "parenting_style": user_info['parenting_style'],
+        "parenting_goal": user_info['parenting_goal'],
+        "child_traits": user_info['child_traits'],
+        "preferred_tone": user_info['preferred_tone'],
+        "language": user_info['language'],
+        "health_issues": user_info['allergies_or_health_issues']
+    }
