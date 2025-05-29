2cb1ac045cbd29c27e9e95570a32dd6c4be7a2d0
Uni Ko <unikohoho@gmail.com>
2025-05-29 21:26:13 +0900

    챗봇 기능 최종 연동 및 문단 출력 확인

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -1,12 +1,12 @@
 from ocr_extraction import extract_text_from_gcs
 
 # 원본 pdf 파일이 올라가 있는 gcp의 bucket 
-bucket_name = "your-bucket-name"
+bucket_name = "uni_s_first_bucket_gcp"
 # bucket 안에 있는 OCR 진행 할 raw pdf 파일명
-pdf_filename = "document1.pdf"
+pdf_filename = "감정코칭.pdf"
 # OCR 결과 저장할 경로와 파일명
-ocr_txt_path = "../data/ocr_paragraphs.txt"
+ocr_csv_path = "data/ocr_paragraphs.csv"
 
 # OCR 수행
-extract_text_from_gcs(bucket_name, pdf_filename, ocr_txt_path)
+extract_text_from_gcs(bucket_name, pdf_filename, ocr_csv_path)
 print("OCR 텍스트 추출 및 저장 완료!")
