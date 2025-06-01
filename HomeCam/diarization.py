fa2c5a5440a67f82988a9ba48e5ed746837d72c9
su11ji04 <113424325+su11ji04@users.noreply.github.com>
2025-06-01 13:36:10 +0900

    Update diarization.py

--- HomeCam/diarization.py
+++ HomeCam/diarization.py
@@ -6,7 +6,7 @@ def load_Huggingface_token_from_file(path="keys/Huggingface_token.txt"):
         return f.read().strip()
         
 def run_diarization(audio_path):
-    token = load_token_token_from_file()
+    token = load_Huggingface_token_from_file()
     pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token=token)
     diarization = pipeline(audio_path, num_speakers=2)
     return diarization
