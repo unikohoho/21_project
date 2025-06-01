233ff96c05fa534a2d8bd5743f0d7723842b13d6
su11ji04 <113424325+su11ji04@users.noreply.github.com>
2025-06-02 01:42:56 +0900

    Update load_user.py

--- HomeCam/load_user.py
+++ HomeCam/load_user.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
+
 def load_user_profile(csv_path: str, user_id: str):
     user_df = pd.read_csv(csv_path)
     user_info = user_df[user_df['user_id'] == user_id].iloc[0]
-
     return {
         "child_age": user_info['child_age'],
         "parenting_style": user_info['parenting_style'],
